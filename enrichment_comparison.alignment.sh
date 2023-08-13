5e8dd7612579f241a3bebec3fcd9a47594ce25af
Salvador Castaneda <cast9836@vandals.uidaho.edu>
2023-08-12 19:13:53 -0700

    update alignment script

--- enrichment_comparison.alignment.sh
+++ enrichment_comparison.alignment.sh
@@ -17,6 +17,6 @@ bam_out=../05_bwa_alignments/enrichment_comparison/unsorted/${base}_aligned.bam 
 sorted=../05_bwa_alignments/enrichment_comparison/sorted/${base}_sorted_aligned.bam #sorted alignment file (by name)
 
 #run fastp, bwa, and samtools
-#fastp -i $R1_in -I $R2_in -o $R1_trimmed -O $R2_trimmed --thread 16 -l 50
+fastp -i $R1_in -I $R2_in -o $R1_trimmed -O $R2_trimmed --thread 16 -l 50
 bwa mem -t 32 -5SPY $genome $R1_trimmed $R2_trimmed | samblaster |samtools view -@ 32 -S -h -b -F 2316 > $bam_out
 samtools sort -o $sorted -n -@ 32 $bam_out #sorted by name rather than position
