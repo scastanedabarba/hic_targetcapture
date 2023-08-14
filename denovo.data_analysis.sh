6a8c9c4deb5436ed0fed11baad1e925e3b08dbf9
Salvador Castaneda <cast9836@vandals.uidaho.edu>
2023-08-14 11:24:14 -0700

    add code for running kraken

--- denovo.data_analysis.sh
+++ denovo.data_analysis.sh
@@ -10,4 +10,19 @@ FILE2=$(basename $FILE)
 
 echo working on $FILE2
 
+#run python script to generate csv files
 python3 denovo.data_analysis.py $FILE2
+
+#specify file paths for kraken ouput
+PREFIX=$(basename $FILE2 _sorted_aligned.bam)
+kraken_report=../06_output/de_novo/${PREFIX}.kreport2
+kraken_out=../06_output/de_novo/${PREFIX}_out.kraken2
+taxonomy_out=../06_output/de_novo/${PREFIX}_lineage.txt
+
+#run kraken and then use taxonkit+csvtk to generate file with taxonomic breakdown
+kraken2 --db ~/kraken_standard --threads 32 --confidence .5 --report $kraken_report $pothosts > $kraken_out
+
+awk -F '\t' '{print $3}' $kraken_out \
+    | taxonkit reformat -I 1 \
+    | csvtk -H -t sep -f 2 -s ';' -R \
+    | csvtk add-header -t -n taxid,kingdom,phylum,class,order,family,genus,species > $taxonomy_out
