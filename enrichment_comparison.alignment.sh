0234a135e0fa4a6cc32a424cd805f7ff09e75a1a
Salvador Castaneda <cast9836@vandals.uidaho.edu>
2023-11-24 16:07:17 -0800

    final enrichment comparison scripts

--- enrichment_comparison.alignment.sh
+++ enrichment_comparison.alignment.sh
@@ -4,15 +4,15 @@
 library=$1
 
 echo $library
-base=$(basename $library _R1.fastq.gz)
+base=$(basename $library _1.fastq.gz)
 echo $base
 
 #specify paths to input and output files
 genome=../02_ref_genomes/combined_genomes.fasta #bwa indexed reference genomes
-R1_in=${base}_R1.fastq.gz #R1
-R2_in=${base}_R2.fastq.gz #R2
-R1_trimmed=../03_trimmed_reads/${base}_R1.fastq.gz #trimmed R1
-R2_trimmed=../03_trimmed_reads/${base}_R2.fastq.gz #trimmed R2
+R1_in=${base}_1.fastq.gz #R1
+R2_in=${base}_2.fastq.gz #R2
+R1_trimmed=../03_trimmed_reads/${base}_1.fastq.gz #trimmed R1
+R2_trimmed=../03_trimmed_reads/${base}_2.fastq.gz #trimmed R2
 bam_out=../05_bwa_alignments/enrichment_comparison/unsorted/${base}_aligned.bam #alignment file
 sorted=../05_bwa_alignments/enrichment_comparison/sorted/${base}_sorted_aligned.bam #sorted alignment file (by name)
 
